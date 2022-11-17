b159780faa16056bc53218e1aac742fd3f72959b
Francesco Castronuovo <118483640+francesco-castronuovo@users.noreply.github.com>
2022-11-17 23:43:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-import vertexShader from './shaders/vertex.glsl'
-import fragmentShader from './shaders/fragment.glsl'
-import atmosphereVertexShader from './shaders/atmospherevertex.glsl'
-import atmosphereFragmentShader from './shaders/atmospherefragment.glsl'
+const vertexShader = 'varying vec2 vertexUV; varying vec3 vertexNormal; void main(){ vertexUV = uv; vertexNormal = normalize(normalMatrix * normal); gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1); }';
+
+const fragmentShader = 'uniform sampler2D globeTexture; varying vec2 vertexUV; varying vec3 vertexNormal; void main() { float intensity = 1.01 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)); vec3 atmosphere = vec3(0.2, 0.4, 1.0) * pow(intensity, 1.9); gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0); }';
+
+const atmosphereVertexShader = 'varying vec3 vertexNormal; void main(){ vertexNormal = normalize(normalMatrix * normal); gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1); }'
+
+const atmosphereFragmentShader = 'varying vec3 vertexNormal; void main() { float intensity = pow(0.87 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)), 2.0);  gl_FragColor = vec4(0.08, 0.15, 0.61, 1.0) * intensity; }';
 
 const canvasContainer = document.querySelector('.canvas-embed');
 
